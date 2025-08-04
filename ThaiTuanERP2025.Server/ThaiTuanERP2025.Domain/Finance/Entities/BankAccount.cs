0f5343ff0e470754cd7c0e43f247d0f820495ae3
trKienDev <trkien.dev@gmail.com>
2025-08-05 06:18:38 +0700

    init module finance migration

---
+++ ThaiTuanERP2025.Server/ThaiTuanERP2025.Domain/Finance/Entities/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThaiTuanERP2025.Domain.Finance.Entities
+{
+	public class BankAccount
+	{
+		public Guid Id { get; set; }
+		public string AccountNumber { get; set; } = null!;
+		public string BankName { get; set; } = null!;
+		public string AccountHolder { get; set; } = null!;
+		public string? EmployeeCode { get; set; }
+
+		public Guid? DepartmentId {  get; set; }
+		public string? CustomerName { get; set; }
+
+		public string? Note { get; set; }
+		public bool IsActive { get; set; } = true;
+		public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+	}
+}
