066f5693c3d3fec13d08c5d7149e9e0b17a7ca95
trKienDev <trkien.dev@gmail.com>
2025-08-05 06:18:38 +0700

    init module finance migration

---
+++ ThaiTuanERP2025.Server/ThaiTuanERP2025.Domain/Finance/Entities/BudgetPeriod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThaiTuanERP2025.Domain.Finance.Entities
+{
+	public class BudgetPeriod
+	{
+		public Guid Id { get; set; }
+		public int Year { get; set; }
+		public int Month { get; set; }
+		public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+		public DateTime? UpdatedDate { get; set; }
+		public bool IsActive { get; set; } = true;
+		public ICollection<BudgetPlan> BudgetPlans { get; set; } = new List<BudgetPlan>();
+	}
+}
