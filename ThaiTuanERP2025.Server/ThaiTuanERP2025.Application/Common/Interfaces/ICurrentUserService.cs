11fcd500ff4471a5179f445b1e53cbccb6627f83
trKienDev <trkien.dev@gmail.com>
2025-08-07 18:04:43 +0700

    init AuditableEntity, BaseEntity & CurrentUserService

---
+++ ThaiTuanERP2025.Server/ThaiTuanERP2025.Application/Common/Interfaces/ICurrentUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThaiTuanERP2025.Application.Common.Interfaces
+{
+	public interface ICurrentUserService
+	{
+		Guid? UserId { get; }
+	}
+}
