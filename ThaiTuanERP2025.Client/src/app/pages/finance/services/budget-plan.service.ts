5ee14b2ef752bedecaf1f65572dba7d4d163cb16
trKienDev <trkien.dev@gmail.com>
2025-08-08 16:41:45 +0700

    budget-plan service

---
+++ ThaiTuanERP2025.Client/src/app/pages/finance/services/budget-plan.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../../../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { BudgetPlanModel, CreateBudgetPlanModel } from "../models/budget-plan.model";
+import { ApiResponse } from "../../../core/models/api-response.model";
+import { handleApiResponse$ } from "../../../core/utils/handle-api-response.operator";
+
+@Injectable({ providedIn: 'root' })
+export class BudgetPlanService {
+      private readonly API_URL = `${environment.apiUrl}/budget-plan`;
+      constructor(private http: HttpClient) {}
+
+      getAll(): Observable<BudgetPlanModel[]> {
+            return this.http.get<ApiResponse<BudgetPlanModel[]>>(`${this.API_URL}/all`)
+                  .pipe(handleApiResponse$<BudgetPlanModel[]>());
+      } 
+
+      create(plan: CreateBudgetPlanModel): Observable<BudgetPlanModel> {
+            return this.http.post<ApiResponse<BudgetPlanModel>>(this.API_URL, plan)
+                  .pipe(handleApiResponse$<BudgetPlanModel>());
+      }
+
+      update(plan: Partial<CreateBudgetPlanModel>): Observable<BudgetPlanModel> {
+            return this.http.put<ApiResponse<BudgetPlanModel>>(this.API_URL, plan)
+                  .pipe(handleApiResponse$<BudgetPlanModel>());
+      }
+
+      
+}
