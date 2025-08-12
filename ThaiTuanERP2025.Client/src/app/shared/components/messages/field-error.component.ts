a54e4968f1c2cb0774af30a272aa2e8be7a9e469
trKienDev <trkien.dev@gmail.com>
2025-08-13 06:27:54 +0700

    alert-message component

---
+++ ThaiTuanERP2025.Client/src/app/shared/components/messages/field-error.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
+import { AbstractControl, NonNullableFormBuilder } from "@angular/forms";
+
+@Component({
+      selector: 'app-field-error',
+      standalone: true,
+      imports: [CommonModule],
+      template: `
+            <div class="alert-message danger-message" role="alert" *ngIf="visible">
+                  <ng-content></ng-content>
+                  <button type="button" class="close-btn" (click)="closes=true" aria-label="Đóng">x</button>
+            </div>
+      `
+})
+export class FieldErrorComponent {
+      @Input() control: AbstractControl | null = null;
+      @Input() submitted = false;
+      closed = false;
+
+      get visible(): boolean {
+            const c = this.control;
+            if(!c || this.closed) return false;
+
+            // Hiện lỗi nếu: (đã submit || user đã tương tác) & control invalid
+            const interacted = c.touched || c.dirty;
+            return (this.submitted || interacted) && c.invalid;
+      }
+}
