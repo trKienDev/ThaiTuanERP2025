7ab8ef1424cc1933f291c0f9464be1b9e1f5cb91
trKienDev <trkien.dev@gmail.com>
2025-08-19 22:44:56 +0700

    feat: api controller of LedgerAccount, Tax, CashOutCode, CashOutGroup

--- ThaiTuanERP2025.Server/ThaiTuanERP2025.Api/Controllers/Finance/LedgerAccountController.cs
+++ ThaiTuanERP2025.Server/ThaiTuanERP2025.Api/Controllers/Finance/LedgerAccountController.cs
@@ -1,6 +1,10 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using ThaiTuanERP2025.Api.Common;
+using ThaiTuanERP2025.Application.Finance.Commands.BankAccounts.ToggleBankAccountStatus;
+using ThaiTuanERP2025.Application.Finance.Commands.LedgerAccounts.CreateLedgerAccount;
+using ThaiTuanERP2025.Application.Finance.Commands.LedgerAccounts.DeleteLedgerAccount;
+using ThaiTuanERP2025.Application.Finance.Commands.LedgerAccounts.UpdateLedgerAccount;
 using ThaiTuanERP2025.Application.Finance.DTOs;
 using ThaiTuanERP2025.Application.Finance.Queries.LedgerAccounts.GetAllLedgerAccounts;
 using ThaiTuanERP2025.Application.Finance.Queries.LedgerAccounts.GetLedgerAccountById;
@@ -17,12 +21,39 @@ namespace ThaiTuanERP2025.Api.Controllers.Finance
 		}
 
 		[HttpGet]
-		public async Task<IActionResult<List<LedgerAccountDto>> GetAll() {
+		public async Task<IActionResult> GetAll() {
 			var dto = await _mediator.Send(new GetAllLedgerAccountsQuery());
-			if(dto == null) {
-				return NotFound(ApiResponse<string>.Fail("Không tìm thấy bất kỳ tài khoản kế toán nào"));
-			}
-			return Ok(ApiResponse<LedgerAccountDto>.Success(dto));
+			return Ok(ApiResponse<List<LedgerAccountDto>>.Success(dto));
 		} 
+
+		[HttpGet("{id:guid}")]
+		public async Task<IActionResult> GetById(Guid id) {
+			var data = await _mediator.Send(new GetLedgerAccountByIdQuery(id));
+			return Ok(ApiResponse<LedgerAccountDto>.Success(data));
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> Create([FromBody] CreateLedgerAccountCommand command) {
+			var result = await _mediator.Send(command);
+			return Ok(ApiResponse<LedgerAccountDto>.Success(result));
+		}
+
+		[HttpPut("{id:guid}")]
+		public async Task<IActionResult> Update(Guid id, [FromBody] UpdateLedgerAccountCommand command) {
+			var result = await _mediator.Send(command with { Id = id});
+			return Ok(ApiResponse<LedgerAccountDto>.Success(result, "Cập nhật thành công"));
+		}
+
+		[HttpPatch("{id:guid}/status")]
+		public async Task<IActionResult> ToggleStatus(Guid id, [FromBody] bool IsActive) {
+			await _mediator.Send(new ToggleBankAccountStatusCommand(id, IsActive));
+			return Ok(ApiResponse<bool>.Success(true));
+		}
+
+		[HttpDelete("{id:guid}")]
+		public async Task<ActionResult> Delete(Guid id) {
+			await _mediator.Send(new DeleteLedgerAccountCommand(id));
+			return Ok(ApiResponse<bool>.Success(true));
+		}
 	}
 }
---
+++ ThaiTuanERP2025.Server/ThaiTuanERP2025.Api/Controllers/Finance/TaxController.cs
@@ -0,0 +1,63 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using ThaiTuanERP2025.Api.Common;
+using ThaiTuanERP2025.Application.Finance.Commands.Taxes.CreateTax;
+using ThaiTuanERP2025.Application.Finance.Commands.Taxes.DeleteTax;
+using ThaiTuanERP2025.Application.Finance.Commands.Taxes.ToggleTaxStatus;
+using ThaiTuanERP2025.Application.Finance.Commands.Taxes.UpdateTax;
+using ThaiTuanERP2025.Application.Finance.DTOs;
+using ThaiTuanERP2025.Application.Finance.Queries.Taxes.GetAllTaxes;
+using ThaiTuanERP2025.Application.Finance.Queries.Taxes.GetTaxById;
+
+namespace ThaiTuanERP2025.Api.Controllers.Finance
+{
+	[ApiController]
+	[Authorize]
+	[Route("api/taxes")]
+	public class TaxController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+		public TaxController(IMediator mediator) {
+			_mediator = mediator;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult> GetAll()
+		{
+			var listTaxes = await _mediator.Send(new GetAllTaxesQuery());
+			return Ok(ApiResponse<List<TaxDto>>.Success(listTaxes));
+		}
+
+		[HttpGet("{id:guid}")]
+		public async Task<ActionResult> GetById(Guid id) {
+			var tax = await _mediator.Send(new GetTaxByIdQuery(id));
+			return Ok(ApiResponse<TaxDto>.Success(tax));
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> Create([FromBody] CreateTaxCommand command) {
+			var result = await _mediator.Send(command);
+			return Ok(ApiResponse<TaxDto>.Success(result));
+		}
+
+		[HttpPut("{id:guid}")]
+		public async Task<ActionResult> Update(Guid id, [FromBody] UpdateTaxCommand command) {
+			var result = await _mediator.Send(command with { Id = id });
+			return Ok(ApiResponse<TaxDto>.Success(result));
+		}
+
+		[HttpPatch("{id:guid}/status")]
+		public async Task<ActionResult> ToggleStatus(Guid id, [FromBody] bool isActive) {
+			var result = await _mediator.Send(new ToggleTaxStatusCommand(id, isActive));
+			return Ok(ApiResponse<bool>.Success(result));
+		}
+
+		[HttpDelete("{id:guid}")]
+		public async Task<ActionResult> Delete(Guid id) {
+			var result = await _mediator.Send(new DeleteTaxCommand(id));
+			return Ok(ApiResponse<bool>.Success(result));
+		}
+	}
+}
