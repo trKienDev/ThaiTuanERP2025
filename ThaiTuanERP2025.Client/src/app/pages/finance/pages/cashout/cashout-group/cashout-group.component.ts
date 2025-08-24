43a32b6210188bc48c3556044ee268da57f4f5e6
trKienDev <trkien.dev@gmail.com>
2025-08-25 06:13:01 +0700

    ux/ui: tax request

---
+++ ThaiTuanERP2025.Client/src/app/pages/finance/pages/cashout/cashout-group/cashout-group.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+      selector: 'finance-cashout-group',
+      standalone: true,
+      imports: [ CommonModule ],
+      templateUrl: './cashout-group.component.html',
+      styleUrl: './cashout-group.component.scss'
+})
+export class CashoutGroupComponent {
+      
+}
