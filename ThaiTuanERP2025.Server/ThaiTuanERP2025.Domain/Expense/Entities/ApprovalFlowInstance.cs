e9eb5ca0eff82596008e10334a8eb6061382716e
trKienDev <trkien.dev@gmail.com>
2025-08-29 17:56:24 +0700

    init entity & schema definition of 5 approval

---
+++ ThaiTuanERP2025.Server/ThaiTuanERP2025.Domain/Expense/Entities/ApprovalFlowInstance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThaiTuanERP2025.Domain.Common;
+using ThaiTuanERP2025.Domain.Expense.Enums;
+
+namespace ThaiTuanERP2025.Domain.Expense.Entities
+{
+	public class ApprovalFlowInstance : AuditableEntity
+	{
+		public Guid FlowDefinitionId { get; set; }
+		public ApprovalFlowDefinition FlowDefinition { get; set; } = default!;
+		public int DefinitionVersion { get; set; }
+
+		public string DocumentType { get; set; } = default!;
+		public Guid DocumentId {  get; set; }
+
+		public ApprovalFlowStatus Status { get; set; } = ApprovalFlowStatus.Pending;
+		public int? CurrentStepIndex { get; set; }	
+		public DateTime? StartedAt { get; set; }	
+		public DateTime? FinishedAt { get; set; }
+
+		public ICollection<ApprovalStepInstance> Steps {  get; set; } = new List<ApprovalStepInstance>();
+		public byte[] RowVersion { get; set; } = default!;
+	}
+}
---
+++ ThaiTuanERP2025.Server/ThaiTuanERP2025.Infrastructure/Approval/Configurations/ApprovalActionConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThaiTuanERP2025.Domain.Expense.Entities;
+
+namespace ThaiTuanERP2025.Infrastructure.Approval.Configurations
+{
+	public class ApprovalActionConfiguration : IEntityTypeConfiguration<ApprovalAction>
+	{
+		public void Configure(EntityTypeBuilder<ApprovalAction> builder)
+		{
+			builder.ToTable("Actions", "Approval");
+			builder.HasKey(x => x.Id);	
+
+			builder.Property(x => x.Action).HasConversion<int>().IsRequired();
+			builder.Property(x => x.Comment).HasMaxLength(2000);
+			builder.Property(x => x.AttachmentFileIdsJson).HasColumnType("nvarchar(max)");
+			builder.Property(x => x.OccuredAt).IsRequired();
+
+			builder.HasIndex(x => new { x.StepInstanceId, x.OccuredAt });
+		}
+	}
+}
