ce45c98d12583e72e989d6b1028f89f21ddd0f3b
trKienDev <trkien.dev@gmail.com>
2025-08-30 12:09:23 +0700

    CQRS: CommentOnStep

---
+++ ThaiTuanERP2025.Server/ThaiTuanERP2025.Application/Expense/Commands/ApprovalWorkflow/CommentOnStep/CommentOnStepCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThaiTuanERP2025.Application.Expense.Commands.ApprovalWorkflow.CommentOnStep
+{
+	public sealed record CommentOnStepCommand(
+		Guid StepInstanceId,
+		string? Comment, // cho phép trống nếu upload file
+		List<Guid>? AttachmentFileIds // optional
+	) : IRequest<Unit>;
+}
