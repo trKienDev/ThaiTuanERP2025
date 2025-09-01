5439cafc5b70bb4ed2c4019f42e1f5714c68c451
trKienDev <trkien.dev@gmail.com>
2025-09-01 17:06:56 +0700

    feat: workflow approval controller

---
+++ ThaiTuanERP2025.Server/ThaiTuanERP2025.Infrastructure/Auth/Requirements/CanCommentStepRequirement.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace ThaiTuanERP2025.Infrastructure.Auth.Requirements
+{
+	public sealed class CanCommentStepRequirement : IAuthorizationRequirement
+	{
+	}
+}
