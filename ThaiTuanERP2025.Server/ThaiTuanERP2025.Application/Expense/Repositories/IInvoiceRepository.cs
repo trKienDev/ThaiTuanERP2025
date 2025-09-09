89470b8aa7735cc3436e03d1b9401cf51d27808f
trKienDev <trkien.dev@gmail.com>
2025-09-09 22:11:43 +0700

    init my invoice dialog

--- ThaiTuanERP2025.Server/ThaiTuanERP2025.Application/Expense/Repositories/IInvoiceRepository.cs
+++ ThaiTuanERP2025.Server/ThaiTuanERP2025.Application/Expense/Repositories/IInvoiceRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ThaiTuanERP2025.Application.Common.Models;
+﻿using ThaiTuanERP2025.Application.Common.Models;
 using ThaiTuanERP2025.Application.Common.Persistence;
 using ThaiTuanERP2025.Application.Expense.Dtos;
 using ThaiTuanERP2025.Domain.Expense.Entities;
@@ -13,5 +8,6 @@ namespace ThaiTuanERP2025.Application.Expense.Repositories
 	public interface IInvoiceRepository : IBaseRepository<Invoice>
 	{
 		Task<PagedResult<InvoiceDto>> GetInvoicesPagedAsync(int page, int pageSize, string? keyword, CancellationToken cancellationToken);
+		Task<PagedResult<InvoiceDto>> GetByCreatorPagedAsync(Guid userId, int page, int pageSize, CancellationToken cancellationToken);
 	}
 }
