1a950efeecce030113d9847432ef5edb7f957d92
trKienDev <trkien.dev@gmail.com>
2025-09-09 13:02:09 +0700

    ux/ui: pick date calendar

--- ThaiTuanERP2025.Client/src/app/app.config.ts
+++ ThaiTuanERP2025.Client/src/app/app.config.ts
@@ -4,7 +4,10 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MondayFirstDateAdapter } from './shared/date/monday-first-date-adapter';
 
 export const appConfig: ApplicationConfig = {
       providers: [
@@ -15,6 +18,16 @@ export const appConfig: ApplicationConfig = {
                         authInterceptor,
                         errorInterceptor
                   ])
-            )
+            ),
+            provideAnimations(),
+            // mat-date-picker
+            { provide: DateAdapter, useClass: MondayFirstDateAdapter },
+            { provide: MAT_DATE_LOCALE, useValue: 'vi-VN' },
+            { provide: MAT_DATE_FORMATS, useValue: {
+                        parse:   { dateInput: 'DD/MM/YYYY' },
+                        display: { dateInput: 'dd/MM/yyyy', monthYearLabel: 'MMMM yyyy', dateA11yLabel: 'dd/MM/yyyy', monthYearA11yLabel: 'MMMM yyyy' },
+                  }
+            },
+            { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { subscriptSizing: 'dynamic' } }
       ]
 };
