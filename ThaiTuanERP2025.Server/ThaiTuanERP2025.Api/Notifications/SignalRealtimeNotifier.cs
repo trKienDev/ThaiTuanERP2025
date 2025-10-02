df4a99d738ab8dd1c949fa8ef3ffe04b6963e1af
trKienDev <trkien.dev@gmail.com>
2025-10-02 16:31:08 +0700

    feat: realtime notification

---
+++ ThaiTuanERP2025.Server/ThaiTuanERP2025.Api/Notifications/SignalRealtimeNotifier.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using ThaiTuanERP2025.Api.Hubs;
+using ThaiTuanERP2025.Application.Notifications.Services;
+
+namespace ThaiTuanERP2025.Api.Notifications
+{
+	public class SignalRealtimeNotifier : IRealtimeNotifier
+	{
+		private readonly IHubContext<NotificationsHub> _hub;
+		public SignalRealtimeNotifier(IHubContext<NotificationsHub> hub)
+		{
+			_hub = hub;
+		}
+
+		public async Task NotifyStepActivatedAsync(IReadOnlyCollection<Guid> targetUserIds, IReadOnlyCollection<object> payloads, CancellationToken cancellationToken = default)
+		{
+			if (targetUserIds.Count == 0 || payloads.Count == 0) return;
+
+			var userIds = targetUserIds.Select(x => x.ToString()).ToList();
+
+			// "ReceiveNotification" là event client-side sẽ lắng nghe
+			await _hub.Clients.Users(userIds).SendAsync("ReceiveNotification", payloads, cancellationToken);
+		}
+	}
+}
