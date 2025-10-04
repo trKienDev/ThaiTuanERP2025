995f7dab8e998998efb361a9f3b9281bb5cddd45
trKienDev <trkien.dev@gmail.com>
2025-10-04 09:20:22 +0700

    feat: task reminder HostedService

--- ThaiTuanERP2025.Server/ThaiTuanERP2025.Application/Notifications/Services/IRealtimeNotifier.cs
+++ ThaiTuanERP2025.Server/ThaiTuanERP2025.Application/Notifications/Services/IRealtimeNotifier.cs
@@ -3,7 +3,7 @@
 	public interface IRealtimeNotifier
 	{
 		Task NotifyStepActivatedAsync(IReadOnlyCollection<Guid> targetUserIds, IReadOnlyCollection<object> payloads, CancellationToken cancellationToken = default);
-		Task PushAlarmsAsync(IEnumerable<Guid> userIds, IEnumerable<object> payloads, CancellationToken cancellationToken = default);
-		Task PushAlarmResolvedAsync(IEnumerable<Guid> userIds, IEnumerable<Guid> alarmIds, CancellationToken cancellationToken = default);
+		Task PushRemindersAsync(IEnumerable<Guid> userIds, IEnumerable<object> payloads, CancellationToken cancellationToken = default);
+		Task PushRemindersResolvedAsync(IEnumerable<Guid> userIds, IEnumerable<Guid> alarmIds, CancellationToken cancellationToken = default);
 	}
 }
