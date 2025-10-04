7f6cfe2f88912b34ea3ebe65e1f16e5788ca9a2f
trKienDev <trkien.dev@gmail.com>
2025-10-04 12:05:50 +0700

    feat: getExpensePaymentDetail Query

--- ThaiTuanERP2025.Server/ThaiTuanERP2025.Api/Controllers/Expense/ExpensePaymentController.cs
+++ ThaiTuanERP2025.Server/ThaiTuanERP2025.Api/Controllers/Expense/ExpensePaymentController.cs
@@ -1,13 +1,16 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ThaiTuanERP2025.Api.Common;
 using ThaiTuanERP2025.Application.Expense.Commands.ExpensePayments.CreateExpensePayments;
 using ThaiTuanERP2025.Application.Expense.Dtos;
+using ThaiTuanERP2025.Application.Expense.Queries.Expense.GetExpensePaymentDetail;
 
 namespace ThaiTuanERP2025.Api.Controllers.Expense
 {
 	[ApiController]
 	[Route("api/expense-payments")]
+	[Authorize]
 	public class ExpensePaymentController : ControllerBase
 	{
 		private readonly IMediator _mediator;
@@ -21,6 +24,15 @@ namespace ThaiTuanERP2025.Api.Controllers.Expense
 		{
 			var result = await _mediator.Send(new CreateExpensePaymentCommand(request), cancellationToken);
 			return Ok(ApiResponse<Guid>.Success(result));
+		}
+
+		[HttpGet("{id:guid}/detail")]
+		[ProducesResponseType(typeof(ApiResponse<ExpensePaymentDetailDto>), StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<IActionResult> GetDetail(Guid id, CancellationToken ct)
+		{
+			var dto = await _mediator.Send(new GetExpensePaymentDetailQuery(id), ct);
+			return Ok(ApiResponse<ExpensePaymentDetailDto>.Success(dto));
 		}
 	}
 }
