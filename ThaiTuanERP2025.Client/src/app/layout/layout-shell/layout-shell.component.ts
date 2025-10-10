f40f9b7d8ad0d78c265cbfcb45ffcf0fee7cbc75
trKienDev <trkien.dev@gmail.com>
2025-10-10 17:55:35 +0700

    ux/ui: toast notification

--- ThaiTuanERP2025.Client/src/app/layout/layout-shell/layout-shell.component.ts
+++ ThaiTuanERP2025.Client/src/app/layout/layout-shell/layout-shell.component.ts
@@ -3,11 +3,12 @@ import { TopbarComponent } from '../topbar/topbar.component';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { ToastNotificationHostComponent } from "../../shared/components/toast-notification/toast-notification-host.component";
 
 @Component({
       selector: 'app-layout-shell',
       standalone: true,
-      imports: [CommonModule, RouterOutlet, SidebarComponent, TopbarComponent],
+      imports: [CommonModule, RouterOutlet, SidebarComponent, TopbarComponent, ToastNotificationHostComponent],
       templateUrl: './layout-shell.component.html',
       styleUrl: './layout-shell.component.scss'
 })
