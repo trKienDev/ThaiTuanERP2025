87a1ec45dfb1a9559ddd6206adb9aeb8a8fc386c
trKienDev <trkien.dev@gmail.com>
2025-10-14 16:15:26 +0700

    ux/ui: ngprogress

--- ThaiTuanERP2025.Client/src/app/layout/layout-shell/layout-shell.component.ts
+++ ThaiTuanERP2025.Client/src/app/layout/layout-shell/layout-shell.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TopbarComponent } from '../topbar/topbar.component';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { ToastNotificationHostComponent } from "../../shared/components/toast-notification/toast-notification-host.component";
 import { loadDomAnimations } from '../../shared/animations/load-dom/load-dom.animation';
+import { ProgressService } from '../../core/services/ngprogress/ngprogress.service';
 
 @Component({
       selector: 'app-layout-shell',
@@ -15,5 +16,17 @@ import { loadDomAnimations } from '../../shared/animations/load-dom/load-dom.ani
       animations: [loadDomAnimations]
 })
 export class LayoutShellComponent {
-      
+      constructor(private router: Router, private progress: ProgressService) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.progress.start();
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        this.progress.done();
+      }
+    });
+  }
 }
