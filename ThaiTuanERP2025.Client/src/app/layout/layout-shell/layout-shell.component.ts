03022bd4158b674bf76b64b53520c33a918efda6
trKienDev <trkien.dev@gmail.com>
2025-10-14 15:48:18 +0700

    ux/ui: add  load-dom animation to kit-shell-tab component

--- ThaiTuanERP2025.Client/src/app/layout/layout-shell/layout-shell.component.ts
+++ ThaiTuanERP2025.Client/src/app/layout/layout-shell/layout-shell.component.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { ToastNotificationHostComponent } from "../../shared/components/toast-notification/toast-notification-host.component";
-import { trigger, transition, query, style, group, animate } from '@angular/animations';
+import { loadDomAnimations } from '../../shared/animations/load-dom/load-dom.animation';
 
 @Component({
       selector: 'app-layout-shell',
@@ -12,32 +12,7 @@ import { trigger, transition, query, style, group, animate } from '@angular/anim
       imports: [CommonModule, RouterOutlet, SidebarComponent, TopbarComponent, ToastNotificationHostComponent],
       templateUrl: './layout-shell.component.html',
       styleUrls: ['./layout-shell.component.scss'],
-      animations: [
-            trigger('routeAnimations', [
-                  transition('* <=> *', [
-                        query(':enter, :leave', [
-                              style({
-                                    position: 'absolute',
-                                    width: '100%',
-                                    opacity: 0,
-                              })
-                        ], { optional: true }),
-
-                        group([
-                              // fade-out nhẹ khi rời khỏi
-                              query(':leave', [
-                                    animate('200ms ease', style({ opacity: 0 }))
-                              ], { optional: true }),
-
-                              // fade-in nhẹ khi vào
-                              query(':enter', [
-                                    style({ opacity: 0 }),
-                                    animate('400ms ease-out', style({ opacity: 1 }))
-                              ], { optional: true }),
-                        ])
-                  ]),
-            ]),
-      ],
+      animations: [loadDomAnimations]
 })
 export class LayoutShellComponent {
       
