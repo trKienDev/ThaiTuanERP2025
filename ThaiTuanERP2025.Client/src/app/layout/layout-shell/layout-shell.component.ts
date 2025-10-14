bc4ad0bf9780c98eef5ca7f4f0ebb0d806484141
trKienDev <trkien.dev@gmail.com>
2025-10-14 15:15:51 +0700

    ux/ui: load module content animation

--- ThaiTuanERP2025.Client/src/app/layout/layout-shell/layout-shell.component.ts
+++ ThaiTuanERP2025.Client/src/app/layout/layout-shell/layout-shell.component.ts
@@ -4,13 +4,40 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { ToastNotificationHostComponent } from "../../shared/components/toast-notification/toast-notification-host.component";
+import { trigger, transition, query, style, group, animate } from '@angular/animations';
 
 @Component({
       selector: 'app-layout-shell',
       standalone: true,
       imports: [CommonModule, RouterOutlet, SidebarComponent, TopbarComponent, ToastNotificationHostComponent],
       templateUrl: './layout-shell.component.html',
-      styleUrl: './layout-shell.component.scss'
+      styleUrls: ['./layout-shell.component.scss'],
+      animations: [
+            trigger('routeAnimations', [
+                  transition('* <=> *', [
+                        query(':enter, :leave', [
+                              style({
+                                    position: 'absolute',
+                                    width: '100%',
+                                    opacity: 0,
+                              })
+                        ], { optional: true }),
+
+                        group([
+                              // fade-out nhẹ khi rời khỏi
+                              query(':leave', [
+                                    animate('200ms ease', style({ opacity: 0 }))
+                              ], { optional: true }),
+
+                              // fade-in nhẹ khi vào
+                              query(':enter', [
+                                    style({ opacity: 0 }),
+                                    animate('400ms ease-out', style({ opacity: 1 }))
+                              ], { optional: true }),
+                        ])
+                  ]),
+            ]),
+      ],
 })
 export class LayoutShellComponent {
       
