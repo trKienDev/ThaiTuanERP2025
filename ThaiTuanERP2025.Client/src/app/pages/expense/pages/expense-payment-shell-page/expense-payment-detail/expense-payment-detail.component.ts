7b19c485503f292da1597e67dd0c193453ac21d5
trKienDev <trkien.dev@gmail.com>
2025-10-14 13:01:51 +0700

    fix: render children component into tab content

---
+++ ThaiTuanERP2025.Client/src/app/pages/expense/pages/expense-payment-shell-page/expense-payment-detail/expense-payment-detail.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+      selector: 'expense-payment-detail',
+      standalone: true,
+      templateUrl: './expense-payment-detail.component.html',
+      imports: [ CommonModule ]
+})
+export class ExpensePaymentDetailPanelComponent { 
+      
+}
