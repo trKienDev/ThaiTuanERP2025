d64e74cd237637b1eb613bf6ad5270e28a02daeb
trKienDev <trkien.dev@gmail.com>
2025-10-24 11:37:26 +0700

    ux/ui: scrollable & payment-nav-bar

--- ThaiTuanERP2025.Client/src/app/features/finance/services/budget-period.service.ts
+++ ThaiTuanERP2025.Client/src/app/features/finance/services/budget-period.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from "@angular/core";
 import { environment } from "../../../../environments/environment";
 import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { ApiResponse } from "../../../shared/models/api-response.model";
-import { handleApiResponse$ } from "../../../shared/operators/handle-api-response.operator";
 import { BaseCrudService } from "../../../shared/services/base-crud.service";
 import { BudgetPeriodDto, BudgetPeriodRequest } from "../models/budget-period.model";
 
 @Injectable({ providedIn: 'root' })
 export class BudgetPeriodService extends BaseCrudService<BudgetPeriodDto, BudgetPeriodRequest> {
-      constructor()
+      constructor(http: HttpClient) {
+            super(http, `${environment.apiUrl}/budget-period`);
+      }
 }
