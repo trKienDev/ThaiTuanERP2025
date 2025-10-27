56102ac129f3a7850e3a0954067a4a037f1e6497
trKienDev <trkien.dev@gmail.com>
2025-10-27 17:52:17 +0700

    ux/ui: assing permission to role dialog

--- ThaiTuanERP2025.Client/src/app/features/account/pages/rbac-shell-page/account-roles/account-role.component.ts
+++ ThaiTuanERP2025.Client/src/app/features/account/pages/rbac-shell-page/account-roles/account-role.component.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from "@angular/common";
 import { Component, inject } from "@angular/core";
-import { MatDialog } from "@angular/material/dialog";
+import { MatDialog, MatDialogRef } from "@angular/material/dialog";
 import { RoleRequestDialogComponent } from "../../../dialogs/role-request-dialog/role-request-dialog.component";
 import { RoleFacade } from "../../../facades/role.facade";
 import { RoleDto } from "../../../models/role.model";
+import { ActionMenuOption } from "../../../../../shared/components/kit-action-menu/kit-action-menu.model";
+import { AssignPermissionToRoleDialogComponent } from "../../../dialogs/assign-permission-to-role-dialog/assign-permission-to-role-dialog.component";
+import { KitActionMenuComponent } from "../../../../../shared/components/kit-action-menu/kit-action-menu.component";
 
 @Component({
       selector: "account-role-panel",
       standalone: true,
       templateUrl: "./account-role.component.html",
-      imports: [ CommonModule ],
+      imports: [CommonModule, KitActionMenuComponent],
 })
 export class AccountRolePanelComponent {
       private matDialog = inject(MatDialog);
@@ -29,4 +32,17 @@ export class AccountRolePanelComponent {
                   }
             });
       }
-}
+
+      buildRoleActions(role: RoleDto) : ActionMenuOption[] {
+            return [
+                  { label: '⚙️ Gán quyền', action: () => this.assignPermissionsToRole(role) },
+            ];
+      }
+      assignPermissionsToRole(role: RoleDto): void {
+            const dialogRef = this.matDialog.open(AssignPermissionToRoleDialogComponent, {
+                  data: role
+            });
+            dialogRef.afterClosed().subscribe();
+      }
+
+}     
