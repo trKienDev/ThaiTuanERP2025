f098c53e655cd0cc96cd7f467c1805511815a99e
trKienDev <trkien.dev@gmail.com>
2025-10-27 16:29:10 +0700

    feat: permission-based authorization

---
+++ ThaiTuanERP2025.Server/ThaiTuanERP2025.Api/Security/AuthorizationPolicyExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using ThaiTuanERP2025.Domain.Account.Entities;
+using ThaiTuanERP2025.Infrastructure.Persistence;
+
+namespace ThaiTuanERP2025.Api.Security
+{
+	public static class AuthorizationPolicyExtensions
+	{
+		public static async Task AddPermissionPoliciesFromDatabaseAsync(this AuthorizationOptions options, IServiceProvider serviceProvider)
+		{
+			using var scope = serviceProvider.CreateScope();
+			var dbContext = scope.ServiceProvider.GetRequiredService<ThaiTuanERP2025DbContext>();
+
+			var permissions = await dbContext.Set<Permission>().AsNoTracking()
+				.Select(p => p.Code)
+				.ToListAsync();
+
+			foreach (var permissionCode in permissions)
+			{
+				options.AddPolicy($"Permission:{permissionCode}", policy => policy.RequireClaim("permission", permissionCode));
+			}
+		}
+	}
+}
