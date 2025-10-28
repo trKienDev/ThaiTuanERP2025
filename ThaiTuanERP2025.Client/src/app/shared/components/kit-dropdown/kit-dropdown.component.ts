3d1157717ed2ed04b84f535cd15f296ebc911e43
trKienDev <trkien.dev@gmail.com>
2025-10-28 16:44:04 +0700

    ux/ui: restore kit-dropdown-component

--- ThaiTuanERP2025.Client/src/app/shared/components/kit-dropdown/kit-dropdown.component.ts
+++ ThaiTuanERP2025.Client/src/app/shared/components/kit-dropdown/kit-dropdown.component.ts
@@ -62,7 +62,6 @@ export class KitDropdownComponent implements ControlValueAccessor, OnChanges {
             } else {
                   this._value = (typeof val === 'string' ? val : null);
                   this.syncLabelFromValue();
-                  this.eRef.nativeElement.classList.toggle('has-value', !!this._value);
             }
       }
       registerOnChange(fn: any): void { this.onChange = fn; }
@@ -159,7 +158,6 @@ export class KitDropdownComponent implements ControlValueAccessor, OnChanges {
             this.onTouched();
             this.selectionChange.emit(opt);
             this.clearFilter(); // Đóng menu rồi thì dọn filter
-            this.eRef.nativeElement.classList.toggle('has-value', !!this._value);
       }
 
       /** Danh sách option đã chọn (giữ thứ tự theo options gốc) */
