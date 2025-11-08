ca2392fe31df42e612030a6901ec315e6f4d4253
trKienDev <trkien.dev@gmail.com>
2025-11-08 12:06:16 +0700

    nothing =))

--- ThaiTuanERP2025.Client/src/app/modules/finance/components/budget-plan-apporvers-dilaog/budget-plan-approvers-dialog.component.ts
+++ ThaiTuanERP2025.Client/src/app/modules/finance/components/budget-plan-apporvers-dilaog/budget-plan-approvers-dialog.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from "@angular/common";
 import { Component, inject } from "@angular/core";
 import { MatDialogRef } from "@angular/material/dialog";
@@ -5,15 +6,18 @@ import { DepartmentOptionStore } from "../../../account/options/department-dropd
 import { KitDropdownComponent } from "../../../../shared/components/kit-dropdown/kit-dropdown.component";
 import { KitSpinnerButtonComponent } from "../../../../shared/components/kit-spinner-button/kit-spinner-button.component";
 import { UserOptionStore } from "../../../account/options/user-dropdown-options.store";
+import { ToastService } from '../../../../shared/components/kit-toast-alert/kit-toast-alert.service';
 
 @Component({
       selector: 'budget-plan-approvers-dialog',
       standalone: true,
-      imports: [CommonModule, KitDropdownComponent, KitSpinnerButtonComponent],
+      imports: [CommonModule, KitDropdownComponent, KitSpinnerButtonComponent, ReactiveFormsModule ],
       templateUrl: './budget-plan-approvers-dialog.component.html',
 })
 export class BudgetPlanApproversDialogComponent {
       private readonly dialog = inject(MatDialogRef<BudgetPlanApproversDialogComponent>);
+      private readonly formBuild = inject(FormBuilder);
+      private readonly toast = inject(ToastService);
       
       private readonly departmentOptionStore = inject(DepartmentOptionStore);
       departmentOptions$ = this.departmentOptionStore.option$;
@@ -22,7 +26,29 @@ export class BudgetPlanApproversDialogComponent {
       userOptions$ = this.userOptionStore.option$;
 
       submitting: boolean = false;
+      showErrors: boolean = true;
 
+      form = this.formBuild.group({
+            approverId: this.formBuild.control<string>("", { nonNullable: true, validators: [ Validators.required ]}),
+            slaHours: this.formBuild.control<number>(8, { nonNullable: true, validators: [ Validators.required ]}),
+            departmentIds: this.formBuild.control<string[]>([], { nonNullable: true, validators: [ Validators.required ]})
+      })
+
+      async onSubmit(): Promise<void> {
+            this.showErrors = true;
+            if(this.form.invalid)  {
+                  this.form.markAllAsTouched();
+                  this.toast.warningRich("Vui lòng điền đẩy đủ thông tin ");
+            }
+
+            try { 
+                  this.submitting = true;
+                  this.form.disable({ emitEvent: false });
+                  
+            }
+
+
+      }
 
       close(result?: any) {
             this.dialog.close();
